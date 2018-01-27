5d18e511767391f411d34c7ac22c9f8447eddf50
Jordan Bird <jordanbird-29@hotmail.co.uk>
2018-01-27 16:59:08 +0000

    Visible Sound Basic Implementation

---
+++ Assets/Scripts/Managers/ParticleManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : Singleton<ParticleManager>
+{
+    public GameObject SoundParticlePrefab;
+}
