3a7f5ec4a6cb675c4b71851761f52c2066ac561f
Matthew Moore <matthewemmoore@googlemail.com>
2018-01-27 17:31:14 +0000

    Cubical prefab updated with furniture with rndomly spawning items in the cubicals

--- Assets/Scripts/RandomPicker.cs
+++ Assets/Scripts/RandomPicker.cs
@@ -8,11 +8,13 @@ public class RandomPicker<T> where T : MonoBehaviour
     private IList<T> spawnObjects { get; set; }
     private int SpawnCount { get; set; }
     private GameObject NewType { get; set; }
+    private bool RandomRotation { get; set; }
 
-    public RandomPicker(GameObject newType, int spawnCount)
+    public RandomPicker(GameObject newType, int spawnCount, bool randomRotation = false)
     {
         NewType = newType;
         SpawnCount = spawnCount;
+        RandomRotation = randomRotation;
     }
 
     // Use this for initialization
@@ -37,7 +39,13 @@ public class RandomPicker<T> where T : MonoBehaviour
 	        GameObject newObject = PrefabUtility.InstantiatePrefab(NewType) as GameObject;
 
             newObject.transform.rotation = oldObject.transform.rotation;
-	        newObject.transform.position = oldObject.transform.position;
+
+	        if (RandomRotation)
+	        {
+	            newObject.transform.Rotate(Vector3.up, UnityEngine.Random.Range(0, 359));
+            }
+
+            newObject.transform.position = oldObject.transform.position;
 	        newObject.transform.parent = oldObject.transform.parent;
 
             GameObject.DestroyImmediate(oldObject);
@@ -48,7 +56,7 @@ public class RandomPicker<T> where T : MonoBehaviour
     {
         if (amount > max)
         {
-            throw new Exception("Argggg");
+            amount = max;
         }
 
         IList<int> numbers = new List<int>();
