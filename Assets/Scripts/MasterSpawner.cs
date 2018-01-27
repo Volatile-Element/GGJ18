1ee32a4dd9560f493df181924a494bf55cfd317c
Matthew Moore <matthewemmoore@googlemail.com>
2018-01-27 00:49:30 +0000

    Spawn script for spawning prefabs on gameobjects with a script set

---
+++ Assets/Scripts/MasterSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterSpawner : MonoBehaviour
+{
+    public RandomPicker<PeopleSpawner> peoplePicker;
+    public GameObject PeoplePrefab;
+    public int PeopleSpawnCount;
+
+    // Use this for initialization
+    void Start () {
+        if (peoplePicker == null)
+        {
+            peoplePicker = new RandomPicker<PeopleSpawner>(PeoplePrefab, PeopleSpawnCount);
+        }
+
+        peoplePicker.CallMe();
+	}
+}
---
+++ Assets/Scripts/RandomPicker.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RandomPicker<T> where T : MonoBehaviour
+{
+    private IList<T> spawnObjects { get; set; }
+    private int SpawnCount { get; set; }
+    private GameObject NewType { get; set; }
+
+    public RandomPicker(GameObject newType, int spawnCount)
+    {
+        NewType = newType;
+        SpawnCount = spawnCount;
+    }
+
+    // Use this for initialization
+    public void CallMe () {
+
+	    if (spawnObjects == null)
+	    {
+            spawnObjects = GameObject.FindObjectsOfType(typeof(T)) as IList<T>;
+	    }
+
+	    if (SpawnCount == 0)
+	    {
+	        SpawnCount = 1;
+	    }
+
+	    var randomNumbers = GetRandomNumbers(SpawnCount, spawnObjects.Count);
+
+	    foreach (int randomNumber in randomNumbers)
+	    {
+	        var oldObject = spawnObjects[randomNumber].gameObject;
+
+	        GameObject newObject = PrefabUtility.InstantiatePrefab(NewType) as GameObject;
+
+            newObject.transform.rotation = oldObject.transform.rotation;
+	        newObject.transform.position = oldObject.transform.position;
+	        newObject.transform.parent = oldObject.transform.parent;
+
+            GameObject.DestroyImmediate(oldObject);
+	    }
+	}
+
+    private IList<int> GetRandomNumbers(int amount, int max)
+    {
+        if (amount > max)
+        {
+            throw new Exception("Argggg");
+        }
+
+        IList<int> numbers = new List<int>();
+
+        UnityEngine.Random.InitState(DateTime.UtcNow.Second);
+
+        for (int i = 0; i < amount; i++)
+        {
+            bool newCollection = true;
+
+            while (newCollection)
+            {
+                int randomNumber = UnityEngine.Random.Range(0, max);
+
+                if (numbers.Contains(randomNumber))
+                {
+                    continue;
+                }
+
+                numbers.Add(randomNumber);
+                newCollection = false;
+            }
+        }
+
+        return numbers;
+    }
+}
