dd49a0a5945be1744279b3c3652793e54ee10389
Jordan Bird <jordanbird-29@hotmail.co.uk>
2018-01-28 11:48:53 +0000

    Sound Player Ready

---
+++ Assets/Scripts/Components/DestroySelfIn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelfIn : MonoBehaviour
+{
+    public float SecondsToLive = 1;
+
+    private void Start()
+    {
+        StartCoroutine(DestroySelf());
+    }
+
+    private IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(SecondsToLive);
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class SoundManager : Singleton<SoundManager>
 {
+    GameObject SoundsContainer;
+
     private void Awake()
     {
         if (!IsMainInstance())
         {
             return;
         }
+
+        SoundsContainer = new GameObject("Sounds Container");
     }
 
-    public void PlaySound()
+    public void PlaySingleFire(string resourceLocation)
     {
-        //TODO: Load from resources.
+        PlaySound(resourceLocation, false);
+    }
+
+    public void PlayLoop(string resourceLocation)
+    {
+        PlaySound(resourceLocation, true);
+    }
+
+    private void PlaySound(string resourcesLocation, bool loop)
+    {
+        var audioClip = Resources.Load<AudioClip>(resourcesLocation);
+
+        var sound = new GameObject($"Sound: {audioClip.name} - Created At: {Time.realtimeSinceStartup}");
+        sound.transform.parent = SoundsContainer.transform.parent;
+
+        var audioSource = sound.AddComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        audioSource.playOnAwake = true;
+        audioSource.loop = loop;
+
+        if (!loop)
+        {
+            var destroySelf = sound.AddComponent<DestroySelfIn>();
+            destroySelf.SecondsToLive = audioClip.length + 0.5f; //A buffer
+        }
+
+        audioSource.Play();
     }
 }
