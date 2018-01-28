eea09348982263260fdbd83a8fbda195cf466097
Jordan <jordanbird-29@hotmail.co.uk>
2018-01-28 05:34:21 +0000

    Increase Spawning Encounters and Cats!

--- Assets/Scripts/Placement Scripts/ExternalEncounterSpawnPoint.cs
+++ Assets/Scripts/Placement Scripts/ExternalEncounterSpawnPoint.cs
@@ -13,4 +13,10 @@ public class ExternalEncounterSpawnPoint : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + new Vector3(0, 0.5f, 0), 0.25f);
+    }
 }
