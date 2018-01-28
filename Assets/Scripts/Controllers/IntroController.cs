a160546b456ebc88d1ff28782a58ce68dbf03524
Jordan Bird <jordanbird-29@hotmail.co.uk>
2018-01-28 01:28:36 +0000

    So I can animate apparently..

---
+++ Assets/Scripts/Controllers/IntroController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroController : MonoBehaviour
+{
+    private UIFade uiFade;
+
+    private void Awake()
+    {
+        uiFade = FindObjectOfType<UIFade>();
+    }
+
+    private void Start ()
+    {
+        SetupIntroText();
+    }
+
+    public void SetupIntroText()
+    {
+        DoActionIn.Create(() => { uiFade.FadeTextInAndOut("It's a Monday.."); }, 0);
+        DoActionIn.Create(() => { uiFade.FadeTextInAndOut("You have a migraine.."); }, 4);
+        DoActionIn.Create(() => { uiFade.FadeTextInAndOut("You need a glass, some water and some pain killers."); }, 8);
+        DoActionIn.Create(() => { uiFade.SetText(""); }, 12);
+        DoActionIn.Create(() => { uiFade.FadeToTransparent(); }, 12);
+
+        DoActionIn.Create(() =>
+        {
+            var animator = FindObjectOfType<MigraineTracker>().GetComponent<Animator>();
+            animator.enabled = true;
+            animator.SetTrigger("Start");
+
+            DoActionIn.Create(() => { animator.enabled = false; }, 14);
+        }, 12);
+    }
+}
